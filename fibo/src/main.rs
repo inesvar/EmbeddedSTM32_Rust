9e4e36a6d05a52f333eaaf63122efc8f9ce96e0e
inesvar <90340779+inesvar@users.noreply.github.com>
2023-02-19 14:28:24 +0100

    Utilisation des crates

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,32 +1,45 @@
+use clap::Parser;
+
+/// Compute Fibonacci suite values
+#[derive(Parser, Debug)]
+#[command(author, about, long_about = None)]
+struct Args {
+    /// The maximal number to print the fibo value of
+    value: u32,
+    /// Print intermediate values
+    #[arg(short, long)]
+    verbose: bool,
+    /// The minimum number to compute
+    #[arg(short, long)]
+    number: Option<u32>,
+}
+
 fn main() {
-    println!("Hello, world!");
-    for n in 0..=50 {
-        if let Some(v) = fibo(n) {
-            println!("fibo({n}) = {}", v);
-        } else {
-            break;
-        }
-        /*match fibo(n) {
-            None => break,
-            Some(fibo_n) => println!("fibo({n}) = {}", fibo_n),
-        }*/
+    let args = Args::parse();
+    if let Some(v) = fibo(args.value) {
+        println!("fibo({}) = {}", args.value, v);
     }
 }
 
 
 fn fibo(n: u32) -> Option<u32> {
+    let args = Args::parse();
+    let min :u32 = args.number.unwrap_or(0);
     if n < 2 {
         return Some(n);
     }
     let mut a: u32 = 0;
     let mut b: u32 = 1;
-    for _ in 2..=n {
+    for i in 2..=n {
         let temp = b;
         match a.checked_add(b) {
             Some(sum) => b = sum,
             _ => return None,
         }
         a = temp;
+        if args.verbose && i >= min && i < args.value { 
+            println!("fibo({i}) = {}", b);
+        }
     }
     Some(b)
 }
