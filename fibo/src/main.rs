d1525f9dee40b2985b55b1dbec0fd8f8e0eb42c6
inesvar <90340779+inesvar@users.noreply.github.com>
2023-02-19 12:34:25 +0100

    valeurs correctes de fibo uniquement

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,21 +1,32 @@
 fn main() {
     println!("Hello, world!");
     for n in 0..=50 {
-        println!("fibo({n}) = {}", fibo(n));
+        if let Some(v) = fibo(n) {
+            println!("fibo({n}) = {}", v);
+        } else {
+            break;
+        }
+        /*match fibo(n) {
+            None => break,
+            Some(fibo_n) => println!("fibo({n}) = {}", fibo_n),
+        }*/
     }
 }
 
 
-fn fibo(n: u32) -> u32 {
+fn fibo(n: u32) -> Option<u32> {
     if n < 2 {
-        return n;
+        return Some(n);
     }
     let mut a: u32 = 0;
     let mut b: u32 = 1;
     for _ in 2..=n {
         let temp = b;
-        b = a.checked_add(b).unwrap();
+        match a.checked_add(b) {
+            Some(sum) => b = sum,
+            _ => return None,
+        }
         a = temp;
     }
-    b
+    Some(b)
 }
