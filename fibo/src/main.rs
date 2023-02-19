55c87dd1e2199248fc6d8e5ed2dda8bcbaab0670
inesvar <90340779+inesvar@users.noreply.github.com>
2023-02-19 12:05:34 +0100

    saturated addition

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
-    for n in 0..=42 {
+    for n in 0..=50 {
         println!("fibo({n}) = {}", fibo(n));
     }
 }
@@ -10,11 +10,11 @@ fn fibo(n: u32) -> u32 {
     if n < 2 {
         return n;
     }
-    let mut a = 0;
-    let mut b = 1;
+    let mut a: u32 = 0;
+    let mut b: u32 = 1;
     for _ in 2..=n {
         let temp = b;
-        b = a + b;
+        b = a.saturating_add(b);
         a = temp;
     }
     b
