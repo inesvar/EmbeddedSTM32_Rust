55dfbd4e52aa5c75d74784ac3836d927f815d5c3
inesvar <90340779+inesvar@users.noreply.github.com>
2023-02-19 11:50:11 +0100

    print change + removed if parenthesis

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
     println!("Hello, world!");
     for n in 0..=42 {
-        print!("fibo({}) = ", n);
-        println!("{}", fibo(n));
+        println!("fibo({n}) = {}", fibo(n));
     }
 }
 
 
 fn fibo(n: u32) -> u32 {
-    if (n < 2) {
+    if n < 2 {
         n
     } else {
         fibo(n-1) + fibo(n-2)
