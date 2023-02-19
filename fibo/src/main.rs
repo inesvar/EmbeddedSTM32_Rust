7591ab9fbc2b6f8fd88c5f4ca0cbf439ca77a288
inesvar <90340779+inesvar@users.noreply.github.com>
2023-02-19 12:02:11 +0100

    Iterative version of fibo

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -8,8 +8,14 @@ fn main() {
 
 fn fibo(n: u32) -> u32 {
     if n < 2 {
-        n
-    } else {
-        fibo(n-1) + fibo(n-2)
+        return n;
     }
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 2..=n {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    b
 }
