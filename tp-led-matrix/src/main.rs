7386cde0f69fd07586c442cedee8b55411486197
inesvar <90340779+inesvar@users.noreply.github.com>
2023-03-27 16:51:20 +0200

    Configuring the hardware

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -2,12 +2,28 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
+use stm32l4xx_hal::{pac, prelude::*};
 use panic_probe as _;
 use defmt_rtt as _;
 
 #[entry]
 fn main() -> ! {
+    let cp = pac::CorePeripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    run(cp, dp)
+}
+
+fn run(_cp: pac::CorePeripherals, dp: pac::Peripherals) -> ! {
+    // Get high-level representations of hardware modules
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+
+    // Setup the clocks at 80MHz using HSI (by default since HSE/MSI are not configured).
+    // The flash wait states will be configured accordingly.
+    let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
+
     defmt::info!("Hello, world!");
     panic!("The program stopped");
 }
