45d2ca8da35a393f51950cef68123989d16d4633
inesvar <90340779+inesvar@users.noreply.github.com>
2023-03-27 15:44:14 +0200

    Displaying setup

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -3,13 +3,13 @@
 
 use cortex_m_rt::entry;
 use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
-
-#[panic_handler]
-fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+use rtt_target::rprintln;
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello, world!");
     panic!("The program stopped");
 }
